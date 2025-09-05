00317dead6356ebe06b91f8836659f59bd6db5c0
Leilay123 <leilayates2002@gmail.com>
2025-09-05 15:06:49 -0500

    wip datatyles

--- datatypes.js
+++ datatypes.js
@@ -1,6 +1,30 @@
 /**
  * DATATYPES:
  * 
- * 0: (introduction)
+ * 0: Datatypes are values that variables can hold on to. Each data type have specific properties that will
+ * affect how it behaves and how it's manipulated
  * 
+ * 1: There's multiple datatypes, and they vary between simple and complex. Simple datatypes are STRING, NUMBER,
+ * and BOOLEAN,
+ * 
+ * 1.5: Complex datatypes are ARRAY, and OBJECT
+ * 
+ * 2: Primitive (or simple) are by copy and complex values are by references.
+ * 
+ */
+
+// TYPES OF SIMPLE DATATYPES //
+
+// STRINGS //
+/**
+ * Strings (text strings) are words (layman's terms). However to make something a string it must be in quotes
+ * either 'single quotes' or "double quotes". Strings are mainly use to store text
+ * 
+ */
+    var string = 'this is a string' // 'this is a string' is a string due to it being in single quotes
+    var stringNum = '456' // '456' is a string because it's in single quotes
+
+// NUMBERS //
+/**
+ * Numbers
  */
